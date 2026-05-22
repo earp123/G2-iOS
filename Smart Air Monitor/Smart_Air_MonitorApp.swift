31f518961e2520ec372c422edb97303107b21803
Sam Rall <33456310+earp123@users.noreply.github.com>
2026-05-22 10:07:39 -0500

    Initial Commit

---
+++ Smart Air Monitor/Smart_Air_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_Air_MonitorApp.swift
+//  Smart Air Monitor
+//
+//  Created by Sam Rall on 5/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Smart_Air_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
